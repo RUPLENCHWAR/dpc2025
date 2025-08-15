b7e45e099a8f7cdda031d2a19005f831ff2dea93
Rohan_Uplenchwar <157471049+RUPLENCHWAR@users.noreply.github.com>
2025-08-15 17:32:47 +0530

    Create day1.java
    day 1 challange

---
+++ day1.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Sort012 {
+
+    public static void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        if (n > 0) {
+            System.out.println("Enter " + n + " elements (0, 1, or 2 only):");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+        }
+
+        sortColors(arr);
+
+        System.out.print("Sorted array: [");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
