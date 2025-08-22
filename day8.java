fc51b3f48649f029802baa720d4043a799c3b7ee
Rohan_Uplenchwar <157471049+RUPLENCHWAR@users.noreply.github.com>
2025-08-22 11:24:42 +0530

    Create day8.java
    Daily programming challenge day8

---
+++ day8.java
@@ -0,0 +1,24 @@
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        s = s.trim();
+
+        String[] words = s.split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" "); 
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // Test Cases
+        System.out.println("\"" + reverseWords("the sky is blue") + "\""); 
+        System.out.println("\"" + reverseWords("  hello world  ") + "\"");
+        System.out.println("\"" + reverseWords("a good   example") + "\""); 
+        System.out.println("\"" + reverseWords("    ") + "\"");             
+        System.out.println("\"" + reverseWords("word") + "\"");             
+    }
+}
